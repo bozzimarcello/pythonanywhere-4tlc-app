4c768f05c2209d9e032db5b6fe9acbcc19fa2501
Marcello Bozzi <bozzimarcello@gmail.com>
2022-05-03 07:37:29 +0200

    Aggiunto una index neutra e istruzioni nei commenti

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,17 @@
+# WTF PIPS
+## pip install flask
+## pip install flask_sqlalchemy
+## pip install mysql-connector
+
+# HOW TO RUN
+## python .\flask_app.py
+
+# PLS REMEMBER
+## You need an happy MySQL running and an user+db created on it
+
+# HAVE FUN
+# Your BM
+
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -5,6 +19,7 @@ import os
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+# connection string config
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="ambientali",
     password="2wmcq-Td-Lhm/(45",
@@ -15,8 +30,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# db engine instantiation
 db = SQLAlchemy(app)
 
+# data model
 class Sensor(db.Model):
 
     __tablename__ = "sensors"
@@ -26,9 +43,13 @@ class Sensor(db.Model):
     humidity = db.Column(db.Float)
     pressure = db.Column(db.Float)
 
-
-@app.route("/", methods=["GET", "POST"])
+# each and every route you web app accepts
+@app.route("/")
 def index():
+    return "Eccoti! Sono ruscito a collegarmi a MySQL. Ora puoi usare i comandi 'creadb', 'importa' e 'visualizza'"
+
+@app.route("/visualizza", methods=["GET", "POST"])
+def visualizza():
     if request.method == "GET":
         return render_template("main_page.html", sensors=Sensor.query.all())
 
@@ -48,12 +69,12 @@ def importazione():
         db.session.add(sensor)
         db.session.commit()
     myfile.close()
-    return "Data imported"
+    return "Dati importati"
 
 @app.route("/creadb")
 def creadb():
     db.create_all()
-    return "Database created"
+    return "Database creato"
 
 
 if __name__ == '__main__':
